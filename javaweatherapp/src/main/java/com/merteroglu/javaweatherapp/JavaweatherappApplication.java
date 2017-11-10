6b91b4bf72ee155d270b37b0bd71f93b52fad588
merteroglu <merteroglu16@gmail.com>
2017-11-10 13:42:44 +0200

    v0
    - Proje oluşturuldu. Vaadin ve spring mvc eklendi

---
+++ javaweatherapp/src/main/java/com/merteroglu/javaweatherapp/JavaweatherappApplication.java
@@ -0,0 +1,12 @@
+package com.merteroglu.javaweatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaweatherappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaweatherappApplication.class, args);
+	}
+}
